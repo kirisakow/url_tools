55607a50043f45e2a61f2f4e1bd58cf6d573557f
Kiril Isakov <kirisakow@users.noreply.github.com>
2022-12-31 02:27:07 +0100

    Unescape any HTML entities, e.g. "&amp;" -> "&"

--- url_unescape.py
+++ url_unescape.py
@@ -1,6 +1,8 @@
+import html
 import os
 import urllib.parse
 url_to_unescape = os.getenv('url_to_unescape')
 url_to_unescape = url_to_unescape.strip('\n')
-unescaped_url = urllib.parse.unquote(url_to_unescape)
+unescaped_url = html.unescape(url_to_unescape)
+unescaped_url = urllib.parse.unquote(unescaped_url)
 print(f'\n{unescaped_url}\n')
