b379a40e9c58c5525fbac2f87b723e6c2e460fac
Kiril Isakov <kirix.isakov@gmail.com>
2023-02-14 23:28:35 +0100

    Add flags so that the three unescape tasks are repeated as many times as they make a difference, not a fixed number of times.

--- url_unescape.py
+++ url_unescape.py
@@ -17,34 +17,48 @@ from utils import JournalLogger
 logging.basicConfig(level=logging.DEBUG)
 jl = JournalLogger(program_name='url_tools')
 
+
 def url_unescape(url_to_unescape=None) -> str:
     if url_to_unescape is None or url_to_unescape.strip() == '':
         jl.print('got an empty value for the required param url_to_unescape')
         return ''
     url_to_unescape = url_to_unescape.strip('\n')
-    regex_patterns = {
-        'html_entity': r'&[a-z]+|(#[0-9]+);',
-        'escaped': r'%[uU]([0-9A-Fa-f]{4})',
-        'percent_encoded': r'%([0-9A-Fa-f]{2})'
+    tasks = {
+        'html_entity': {'regex': r'&[a-z]+|(#[0-9]+);', 'repeat': True},
+        'escaped': {'regex': r'%[uU]([0-9A-Fa-f]{4})', 'repeat': True},
+        'percent_encoded': {'regex': r'%([0-9A-Fa-f]{2})', 'repeat': True}
     }
     jl.print(f'url_to_unescape: {url_to_unescape}')
-    how_many_passes = 3
-    for _ in range(how_many_passes):
-        if re.search(regex_patterns['html_entity'], url_to_unescape):
+    while all([task['repeat'] == True for task in tasks.values()]):
+        if (tasks['html_entity']['repeat'] == True
+                and re.search(tasks['html_entity']['regex'], url_to_unescape)):
             jl.print(f'apply html.unescape()')
-            url_to_unescape = html.unescape(url_to_unescape)
-            jl.print(f'result: {url_to_unescape}')
-        if re.search(regex_patterns['escaped'], url_to_unescape):
-            jl.print(
-                f"apply re.sub(regex_patterns['escaped'], lambda m: chr(int(m.group(1), 16)), url_to_unescape)")
-            url_to_unescape = re.sub(regex_patterns['escaped'],
-                                     lambda m: chr(int(m.group(1), 16)),
-                                     url_to_unescape)
-            jl.print(f'result: {url_to_unescape}')
-        if re.search(regex_patterns['percent_encoded'], url_to_unescape):
-            jl.print(f'apply urllib.parse.unquote(url_to_unescape)')
-            url_to_unescape = urllib.parse.unquote(url_to_unescape)
-            jl.print(f'result: {url_to_unescape}')
+            result = html.unescape(url_to_unescape)
+            jl.print(f'result: {result}')
+            if result != url_to_unescape:
+                url_to_unescape = result
+            else:
+                tasks['html_entity']['repeat'] = False
+        if (tasks['escaped']['repeat'] == True
+                and re.search(tasks['escaped']['regex'], url_to_unescape)):
+            jl.print(f"resolve escaped characters (%uXXXX)")
+            result = re.sub(tasks['escaped']['regex'],
+                            lambda m: chr(int(m.group(1), 16)),
+                            url_to_unescape)
+            jl.print(f'result: {result}')
+            if result != url_to_unescape:
+                url_to_unescape = result
+            else:
+                tasks['escaped']['repeat'] = False
+        if (tasks['percent_encoded']['repeat'] == True
+                and re.search(tasks['percent_encoded']['regex'], url_to_unescape)):
+            jl.print(f"resolve percent-encoded characters (%XX)")
+            result = urllib.parse.unquote(url_to_unescape)
+            jl.print(f'result: {result}')
+            if result != url_to_unescape:
+                url_to_unescape = result
+            else:
+                tasks['percent_encoded']['repeat'] = False
     return url_to_unescape
 
 
